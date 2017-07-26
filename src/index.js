c1ab4031c66be8e4372e371031a1f9f77b56a20a
Brandon <brandonyoyoslade@gmail.com>
2017-07-26 08:52:46 -0600

    feat(backend): add directions and menu pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,21 @@ app.set('views', path.join(__dirname, '../templates'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {
+        title: 'Express Burgers'
+    });
+});
+app.get('/directions', function(req, res) {
+    res.render('directions', {
+        title: 'Express Burgers - Directions'
+    });
+});
+app.get('/:menu', function(req, res) {
+    let menu = req.params['menu'];
+    res.render('menu', {
+        title: `Express Burgers - Menu`,
+        menu: menu
+    });
 });
 
 app.listen(3000);
