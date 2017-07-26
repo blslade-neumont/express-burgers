5b02bc37184ef1f59d4f1969863f0f8a72ef849b
Brandon <brandonyoyoslade@gmail.com>
2017-07-26 08:43:28 -0600

    feat(*): add initial layout and index templates; serve using express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+let express = require('express'),
+    pug     = require('pug'),
+    path    = require('path');
 
+var app = express();
 
-console.log(`It's working! It's working!`);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../templates'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
